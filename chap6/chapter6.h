7cbf3a7f43de1163b9e9f38163412fb34d354f46
Yuchen <wangyuchen2121@gmail.com>
2016-09-24 12:16:23 -0400

    add

---
+++ chap6/chapter6.h
@@ -0,0 +1,11 @@
+//
+// Created by Yuchen on 2016-09-23.
+//
+
+#ifndef CHAP6_CHAPTER6_H
+#define CHAP6_CHAPTER6_H
+
+int fact(int val);
+
+
+#endif //CHAP6_CHAPTER6_H
---
+++ chap6/question6_10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void reset(int *i,int *j ){
+    int change = *i;
+    *i = *j;
+    *j = change;
+}
+
+int main() {
+
+    int a = 1;
+    int b = 2;
+    reset(&a,&b);
+    cout << "a = " << a <<endl;
+    cout << "b = " << b <<endl;
+
+
+    return 0;
+}
